9072de68500323c98570f00ef917d8df7d67f058
Sandro <sandrocoelho2023a@gmail.com>
2025-08-18 22:07:32 -0300

    estrutura html das paginas

--- paginaPessoal/pagina/urls.py
+++ paginaPessoal/pagina/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("projetos/", views.projetos, name="projetos"),
+    path("cursos/", views.cursos, name="cursos"),
+    path("recados/", views.recados, name="recados")
 ]
--- paginaPessoal/pagina/views.py
+++ paginaPessoal/pagina/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from .models import Projetos, Recados, Cursos
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "pagina/index.html")
+    projetos = Projetos.objects.all()
+    cursos = Cursos.objects.all()
+    return render(request, "pagina/index.html", {"projetos": projetos, "cursos": cursos})
+
+
+def login(request):
+    return render(request, 'pagina/login.html')
+
+
+def logout(request):
+    pass
+
+
+def projetos(request):
+    return render(request, 'pagina/projetos.html')
+
+
+def cursos(request):
+    return render(request, 'pagina/cursos.html')
+
+
+def recados(request):
+    recados = Recados.objects.all()
+    return render(request, 'pagina/recados.html', {"recados": recados})
