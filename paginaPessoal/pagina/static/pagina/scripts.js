297095bdecbd5c73b966fc987285dfbab386d13b
Sandro <sandrocoelho2023a@gmail.com>
2025-08-20 17:06:26 -0300

    inserção de javaScript e links e redes sociais

--- paginaPessoal/pagina/static/pagina/scripts.js
+++ paginaPessoal/pagina/static/pagina/scripts.js
@@ -1,12 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let editBtns = document.querySelectorAll('.editBtn'); // pega todos os botões
-    let parte13 = document.querySelector('#parte13');     // formulário alvo
+var btnPytohn = document.querySelector('#btnPython');
+var btnJavaScript = document.querySelector('#btnJavaScript');
+var btnDocker = document.querySelector('#btnDocker');
+var btnDjango = document.querySelector('#btnDjango');
+console.log(btnPytohn)
+var timer;
 
-    editBtns.forEach(btn => {
-        btn.addEventListener('click', function (e) {
-            e.preventDefault();
-            // alterna entre mostrar e ocultar
-            parte13.classList.toggle('hide');
-        });
-    });
-});
+
+var pytohn = document.querySelector('#respostaPython');
+var javaScript = document.querySelector('#respostaJavaScript');
+var docker = document.querySelector('#respostaDocker');
+var django = document.querySelector('#respostaDjango');
+
+btnPytohn.addEventListener('mouseover', function () {
+
+    pytohn.classList.remove('hide');
+
+})
+
+btnPytohn.addEventListener('mouseout', function () {
+
+    pytohn.classList.add('hide');
+    
+})
+
+
+btnJavaScript.addEventListener('mouseover', function () {
+    javaScript.classList.remove('hide');
+   
+
+})
+
+btnJavaScript.addEventListener('mouseout', function () {
+    javaScript.classList.add('hide');
+})
+
+btnDocker.addEventListener('mouseover', function () {
+    docker.classList.remove('hide');
+})
+
+btnDocker.addEventListener('mouseout', function () {
+    docker.classList.add('hide');
+})
+
+btnDjango.addEventListener('mouseover', function () {
+    django.classList.remove('hide');
+})
+
+btnDjango.addEventListener('mouseout', function () {
+    django.classList.add('hide');
+})
